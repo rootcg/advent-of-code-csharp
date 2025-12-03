63761d6eab565a9a24abde36cd1ab9c35cd02d69
Cristian <cristian.gonzalez.morante@gmail.com>
2025-12-03 10:30:13 +0100

    initial message

---
+++ AdventOfCode/Day01.cs
@@ -0,0 +1,9 @@
+﻿namespace AdventOfCode;
+
+public class Day01
+{
+    public int Run()
+    {
+        return 1;
+    }
+}
---
+++ AdventOfCode/Main.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode;
+
+public class Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
