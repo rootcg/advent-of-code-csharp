129fbe01f72c788db28b883f00e48e48ec567559
rootcgm <cristian.gonzalez.morante@gmail.com>
2025-12-10 17:49:02 +0100

    solve day 7

---
+++ AdventOfCode.Tests/Puzzles/Day07Test.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Linq;
+using AdventOfCode.Puzzles;
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Tests.Puzzles;
+
+[TestClass]
+[TestSubject(typeof(Day07))]
+public class Day07Test
+{
+
+    private readonly Day07 _program = new();
+    
+    [TestMethod]
+    public void first_exercise_example()
+    {
+        var input = File.ReadAllLines("07.txt");
+        Assert.AreEqual(21, _program.First(input));
+    }
+    
+    [TestMethod]
+    public void second_exercise_example()
+    {
+        var input = File.ReadAllLines("07.txt");
+        Assert.AreEqual(40, _program.Second(input));
+    }
+}
---
+++ AdventOfCode/Puzzles/Day07.cs
@@ -0,0 +1,109 @@
+﻿namespace AdventOfCode.Puzzles;
+
+public class Day07 : IDailyPuzzle
+{
+    private class Beam(int row, int col)
+    {
+        public int Row = row;
+        public readonly int Col = col;
+
+        public void Move()
+        {
+            Row += 1;
+        }
+    }
+    
+    private class QuantumBeam(int row, int col, long timelines) : Beam(row, col)
+    {
+        public long Timelines = timelines;
+
+        public void AddTimelines(long n)
+        {
+            Timelines += n;
+        }
+    }
+
+    public long First(string[] input)
+    {
+        var splitCount = 0;
+        
+        var beams = new List<Beam> { new(0, input[0].IndexOf('S')) };
+        for (var i = 0; i < input.Length - 1; i++)
+        {
+            var j = 0;
+            var beamsCounter = beams.Count;
+            var beamsChecked = 0;
+            while (j < beams.Count && beamsChecked < beamsCounter)
+            {
+                var beam = beams[j];
+                if (input[beam.Row + 1][beam.Col] == '^')
+                {
+                    beams.RemoveAt(j);
+                    AddBeam(new Beam(beam.Row + 1, beam.Col - 1));
+                    AddBeam(new Beam(beam.Row + 1, beam.Col + 1));
+                    splitCount++;
+                }
+                else
+                {
+                    beam.Move();
+                    j++;
+                }
+
+                beamsChecked++;
+            }
+        }
+        
+        return splitCount;
+        
+        void AddBeam(Beam beam)
+        {
+            if (beams.Any(it => it.Col == beam.Col))
+                return;
+            
+            beams.Add(beam);
+        }
+    }
+    
+    public long Second(string[] input)
+    {
+        var beams = new List<QuantumBeam> { new(0, input[0].IndexOf('S'), 1) };
+        for (var i = 0; i < input.Length - 1; i++)
+        {
+            var j = 0;
+            var beamsCounter = beams.Count;
+            var beamsChecked = 0;
+            while (j < beams.Count && beamsChecked < beamsCounter)
+            {
+                var beam = beams[j];
+                if (input[beam.Row + 1][beam.Col] == '^')
+                {
+                    beams.RemoveAt(j);
+                    AddBeam(new QuantumBeam(beam.Row + 1, beam.Col - 1, beam.Timelines));
+                    AddBeam(new QuantumBeam(beam.Row + 1, beam.Col + 1, beam.Timelines));
+                }
+                else
+                {
+                    beam.Move();
+                    j++;
+                }
+
+                beamsChecked++;
+            }
+        }
+
+        return beams.Select(it => it.Timelines).Sum();
+
+        void AddBeam(QuantumBeam beam)
+        {
+            var existingBeam = beams.Find(it => it.Col == beam.Col);
+            if (existingBeam != null)
+            {
+                existingBeam.AddTimelines(beam.Timelines);
+            }
+            else
+            {
+                beams.Add(beam);   
+            }
+        }
+    }
+}
